877343d1d4e2431c9039860dfbdadae37521ed98
Júlia Galvão <juliagalvaoalmeidasantos@gmail.com>
2026-02-12 22:14:12 -0300

    Adiciona funções de copiar, colar e bateria nas notas
    Inclui botões para copiar, colar e consultar nível da bateria no painel de notas. Implementa métodos assíncronos no backend para integração com clipboard e bateria via JavaScript. Expande suporte a recursos nativos do navegador, melhorando a experiência do usuário.

--- WasmMp3.Client/Services/DeviceService.cs
+++ WasmMp3.Client/Services/DeviceService.cs
@@ -15,4 +15,13 @@ public class DeviceService
 
     public ValueTask RegisterOnlineListenerAsync(IJSObjectReference dotNetObjRef)
         => _js.InvokeVoidAsync("device.onOnline", dotNetObjRef);
+
+    //metodos do CopyText (clipboard)
+    public ValueTask<bool> CopyTextAsync(string txt)
+        => _js.InvokeAsync<bool>("clipboard.copyText", txt);
+     public ValueTask<string> ReadTextAsync()
+        => _js.InvokeAsync<string>("clipboard.readText");
+
+    public ValueTask<int> getLevelAsync()
+        => _js.InvokeAsync<int>("battery.getLevel");
 }
--- WasmMp3.Client/wwwroot/device.js
+++ WasmMp3.Client/wwwroot/device.js
@@ -11,3 +11,53 @@ window.deviceStorage = {
         localStorage.removeItem(key);
     }
 };
+
+
+//device access
+
+window.device = {
+    isOnline: () => navigator.onLine,
+    vibrate: (ms) => {
+        if (navigator.vibrate) navigator.vibrate(ms);
+    },
+
+    onOnline: (dotNetObjRef) => {
+        window.addEventListener("online", () => {
+            dotNetObjRef.invokeMethodAsync("OnOnlineChanged", true);
+        });
+        window.addEventListener("offline", () => {
+            dotNetObjRef.invokeMethodAsync("OnOnlineChanged", false);
+        });
+    }
+};
+
+//Clipboard (copiar/colar)
+window.clipboard = {
+    copyText: function (text) {
+        try {
+            navigator.clipboard.writeText(text);
+            return true;
+        } catch (err) {
+            console.error("Erro ao copiar: ", err);
+            return false;
+        }
+    },
+    readText: function () {
+        try {
+            const text = navigator.clipboard.readText();
+            return text;
+        } catch (err) {
+            console.error("Erro ao ler: ", err);
+            return null;
+        }
+    }
+};
+
+//porcentagem da bateria
+window.battery = {
+    getLevel: async function () {
+        if (!navigator.getBattery) return -1;
+        const battery = await navigator.getBattery();
+        return battery.level * 100; // Retorna 0 a 100
+    }
+};
